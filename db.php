5591f7f47079096e8be06270ce1252ba7a823650
jromero <jromero@summasolutions-net>
2021-09-09 20:38:28 -0300

    Tareas completo

---
+++ db.php
@@ -0,0 +1,39 @@
+<?php
+
+function connectToDB(){
+    return new PDO('mysql:host=localhost;'.'dbname=db_tasks2021;charset=utf8', 'root', '');
+}
+
+function getTasks(){
+    $db = connectToDB();
+    $sentencia = $db->prepare( "select * from tareas");
+    $sentencia->execute();
+    $tareas = $sentencia->fetchAll(PDO::FETCH_OBJ);
+    return $tareas;
+}
+
+function getTask($id){
+    $db = connectToDB();
+    $sentencia = $db->prepare( "select * from tareas WHERE id_tarea=?");
+    $sentencia->execute(array($id));
+    $tarea = $sentencia->fetch(PDO::FETCH_OBJ);
+    return $tarea;
+}
+
+function insertTask($mermelada, $descripcion, $prioridad, $finalizada){
+    $db = connectToDB();
+    $sentencia = $db->prepare("INSERT INTO tareas(titulo, descripcion, prioridad, finalizada) VALUES(?, ?, ?, ?)");
+    $sentencia->execute(array($mermelada,$descripcion,$prioridad, $finalizada ));
+}
+
+function deleteTaskFromDB($id){
+    $db = connectToDB();
+    $sentencia = $db->prepare("DELETE FROM tareas WHERE id_tarea=?");
+    $sentencia->execute(array($id));
+}
+
+function updateTaskFromDB($id){
+    $db = connectToDB();
+    $sentencia = $db->prepare("UPDATE tareas SET finalizada=1 WHERE id_tarea=?");
+    $sentencia->execute(array($id));
+}
--- tasks.php
+++ tasks.php
@@ -15,10 +15,18 @@ function showHome(){
         
         <h1>Tareas 2021</h1>
     
-        <ul>
-            <li>Tarea 1</li>
-            <li>Tarea 2</li>
-            <li>Tarea 3</li>
+        <ul>';
+
+    $tasks = getTasks();
+    foreach($tasks as $tarea) {
+        if($tarea->finalizada == 1){
+            $html.= '<li> <strike>'. $tarea->titulo .': '.$tarea->descripcion .' - '.'<a href="deleteTask/'.$tarea->id_tarea.'">Borrar</a> - <a href="updateTask/'.$tarea->id_tarea.'">Done</a>'.'</strike></li>';
+        }else{
+            $html.= '<li> <a href="viewTask/'.$tarea->id_tarea.'">'. $tarea->titulo .'</a>: '.$tarea->descripcion .' - '.'<a href="deleteTask/'.$tarea->id_tarea.'">Borrar</a> - <a href="updateTask/'.$tarea->id_tarea.'">Done</a>'.'</li>';
+        }
+   }
+
+    $html .=   '
         </ul>
      
     
@@ -28,13 +36,57 @@ function showHome(){
             <input type="number" name="priority" id="priority">
             <input type="checkbox" name="done" id="done">
             <input type="submit" value="Guardar">
-    
         </form>
     
     </body>
     </html>';
 
     echo $html;
+
+}
+
+
+function createTask(){
+
+    if(!isset($_POST['done'])){
+        $done = 0;
+    }else{
+        $done = 1;
+    }
+    insertTask($_POST['title'], $_POST['description'], $_POST['priority'], $done);
+    header("Location: ".BASE_URL."home");
+}
+
+function deleteTask($id){
+    deleteTaskFromDB($id);
+    header("Location: ".BASE_URL."home");
+}
+
+function updateTask($id){
+    updateTaskFromDB($id);
+    header("Location: ".BASE_URL."home");
+}
+
+function viewTask($id){
+    $tarea = getTask($id);
+   
+    echo '<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <base href="'.BASE_URL.'" />
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Tareas 2021</title>
+    </head>
+    <body>
+        
+        <h1>Titulo: '.$tarea->titulo.'</h1>
+        <h2>Descripcion: '.$tarea->descripcion.'</h2>
+        <h2>Prioridad: '.$tarea->prioridad.'</h2>
+        <h2>Finalizada: '.$tarea->finalizada.'</h2>
+    </body>
+    </html>';
 
 }
 
